6e4efeeab2df448dbbacde7ed91c899440bed6ef
Jankin <jyluckily@gmail.com>
2019-11-08 15:40:55 +0800

    Initial Style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Xianyadan</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://xianyadan.xyz">咸鸭蛋</a> 的主页。
+    </p>
+    <p>一切，将从这里开始。</p>
+    <p>
+      <a href="#" role="button" class="btn btn-lg btn-success">现在注册</a>
+    </p>
+  </div>
 @stop
