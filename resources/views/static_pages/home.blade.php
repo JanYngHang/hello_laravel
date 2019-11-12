d790b389dc75fb89df74a17e3f3c40f206832b99
Jankin <jyluckily@gmail.com>
2019-11-12 11:31:25 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,7 +7,8 @@
     </p>
     <p>一切，将从这里开始。</p>
     <p>
-      <a href="#" role="button" class="btn btn-lg btn-success">现在注册</a>
+      <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
     </p>
   </div>
+
 @stop
