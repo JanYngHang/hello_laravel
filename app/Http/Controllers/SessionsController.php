1b898857e97f6786d5b838c6407c8748f02c51a2
Jankin <jyluckily@gmail.com>
2019-12-06 17:08:30 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,34 +8,42 @@ use Auth;
 class SessionsController extends Controller
 {
     public function __construct()
-    {   
+    {
         $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
-    
-    public function create() {
+
+    public function create()
+    {
         return view('sessions.create');
     }
 
-    public function store(Request $request) {
-        $credentials = $this->validate($request ,[
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', [Auth::user()]);
-            return redirect()->intended($fallback);
-        }
-        else {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', [Auth::user()]);
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '您的账号尚未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
+        } else {
             session()->flash('danger', '很抱歉，您输入的邮箱或密码有误');
             return redirect()->back()->withInput();
         }
     }
 
-    public function destroy() {
+    public function destroy()
+    {
         Auth::logout();
         session()->flash('success', '您已成功退出！');
         return redirect('/');
