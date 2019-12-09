87ce4e79afedb6408d5e8304f46e79be05b69daa
Jankin <jyluckily@gmail.com>
2019-12-09 14:45:48 +0800

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.default')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Xianyadan</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://xianyadan.xyz">咸鸭蛋</a> 的主页。
-    </p>
-    <p>一切，将从这里开始。</p>
-    <p>
-      <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
-    </p>
+@if(Auth::check())
+<div class="row">
+  <div class="col-md-8">
+    <section class="status_form">
+      @include('shared._status_form')
+    </section>
   </div>
-
+  <aside class="col-md-4">
+    <section class="user_info">
+      @include('shared._user_info', ['user' => Auth::user()])
+    </section>
+  </aside>
+</div>
+@else
+<div class="jumbotron">
+  <h1>Hello Xianyadan</h1>
+  <p class="lead">
+    你现在所看到的是 <a href="https://xianyadan.xyz">咸鸭蛋</a> 的主页。
+  </p>
+  <p>一切，将从这里开始。</p>
+  <p>
+    <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
+  </p>
+</div>
+@endif
 @stop
